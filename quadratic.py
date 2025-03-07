5aaed674d90a027c36be9ab9a68a25d85e872a21
RushanJi <rushangoltay@gmail.com>
2025-03-08 02:32:52 +0700

    Added Better Simplification, Changed The Colors, Handled Linear Equation, Handled a,b,c being string, Made Max Decimal Places 4, Disabled Display Of Dividing and Multiplying 1 and Adding and Subtracting 0.

--- quadratic.py
+++ quadratic.py
@@ -1,18 +1,26 @@
 import math
 
-a = float(input("\033[31mEnter the coefficient a: \033[0m"))
-b = float(input("\033[31mEnter the coefficient b: \033[0m"))
-c = float(input("\033[31mEnter the coefficient c: \033[0m"))
-
-# Comment
-# * Lighter
-# TODO implement inf uture
-#! error
-# ? query liek a question
-
-while a == 0:
-    print("You cant divide by zero. Input a again")
-    a = float(input("Enter the coefficient a: "))
+a = input("\033[32mEnter the coefficient a: \033[0m")
+while isinstance(a, str) == True:
+    try:
+        a = float(a)
+    except ValueError:
+        print("Give A Valid Value")
+        a = input("\033[32mEnter the coefficient a: \033[0m")
+b = input("\033[32mEnter the coefficient b: \033[0m")
+while isinstance(b, str) == True:
+    try:
+        b = float(b)
+    except ValueError:
+        print("Give A Valid Value")
+        b = input("\033[32mEnter the coefficient b: \033[0m")
+c = input("\033[32mEnter the coefficient c: \033[0m")
+while isinstance(c, str) == True:
+    try:
+        c = float(c)
+    except ValueError:
+        print("Give A Valid Value")
+        c = input("\033[32mEnter the coefficient c: \033[0m")
 
 discriminant = b**2 - 4*a*c
 
@@ -62,31 +70,38 @@ def integ(value):
 
 denominator = 2 * a
 
-if discriminant >= 0:
+if a !=0 and discriminant >= 0:
 
     print("\n\033[34m========REAL SOLUTIONS========\033[0m")
     print(f"\033[33mSolution 1: \033[0m")
-    print(f"= {integ(-b)} + {radic(integ(discriminant))} / {integ(denominator)}")
-    print(f"= {integ(-b / denominator)} + {deone(integ(factor / denominator))}{lradic(integ(discriminant))} = {integ(-b / denominator + discriminant**0.5 / denominator)}")
+    print(f"= {integ(-b)} + {radic(integ(round(discriminant, 4)))} / {integ(round(denominator, 4))}")
+    print(f"= {integ(round(-b / denominator, 4))} + {deone(integ(round(factor / denominator, 4)))}√{lradic(integ(discriminant))}")
+    print(f"= {integ(round(-b / denominator + discriminant**0.5 / denominator, 4))}")
 
     print(f"\033[33mSolution 2: \033[0m")
-    print(f"= {integ(-b)} - {radic(integ(discriminant))} / {integ(denominator)}")
-    print(f"= {integ(-b / denominator)} - {deone(integ(factor / denominator))}{lradic(integ(discriminant))} = {integ(-b / denominator - discriminant**0.5 / denominator)}")
+    print(f"= {integ(-b)} - {radic(integ(round(discriminant, 4)))} / {integ(round(denominator, 4))}")
+    print(f"= {integ(round(-b / denominator, 4))} - {deone(integ(round(factor / denominator, 4)))}√{lradic(integ(discriminant))}")
+    print(f"= {integ(round(-b / denominator - discriminant** 0.5 / denominator, 4))}")
     
-elif discriminant == 0:
+elif a != 0 and discriminant == 0:
     print("\n\033[34m========SOLUTION========\033[0m")
     print(f"= {integ(-b)} / {integ(denominator)}")
-    print(f" = {integ(-b / denominator + discriminant**0.5 / denominator)}")
-    
+    print(f" = {integ(round(-b / denominator + discriminant**0.5 / denominator, 4))}")
+
+elif a == 0:
+    print("\n\033[34m========SOLUTION========\033[0m")
+    print(f"= {integ(-b)} / {integ(2)}")
+    print(f" = {integ(round(-b / 2, 4))}")
+
 else:
 
     print("\n\033[34m========COMPLEX SOLUTIONS========\033[0m")
     print(f"\033[33mSolution 1: \033[0m")
-    print(f"= {integ(-b)} + {integ(abs(discriminant))}i / {integ(denominator)}")
-    print(f"= {integ(-b / denominator)} + {radic(integ(abs(discriminant)))}i")
-    print(f"= {integ(-b / denominator)} + {integ(abs(discriminant)**0.5 / denominator)}i")
+    print(f"= {integ(-b)} + {radic(integ(abs(discriminant)))}i / {integ(denominator)}")
+    print(f"= {integ(round(-b / denominator, 4))} + {deone(integ(round(factor / denominator, 4)))}√{lradic(abs(integ(discriminant)))}i")
+    print(f"= {integ(round(-b / denominator, 4))} + {integ(round(abs(discriminant)**0.5 / denominator, 4))}i")
     
     print(f"\033[33mSolution 2: \033[0m")
     print(f"= {integ(-b)} - {radic(integ(abs(discriminant)))}i / {integ(denominator)}")
-    print(f"= {integ(-b / denominator)} - √{deone(integ(factor / denominator))}{lradic(abs(integ(discriminant)))}i")
-    print(f"= {integ(-b / denominator)} - {integ(abs(discriminant)**0.5 / denominator)}i")
+    print(f"= {integ(round(-b / denominator, 4))} - {deone(integ(round(factor / denominator, 4)))}√{lradic(abs(integ(discriminant)))}i")
+    print(f"= {integ(round(-b / denominator, 4))} - {integ(round(abs(discriminant)**0.5 / denominator, 4))}i")
